63679019dd150256cfa08a0e3c68d14ebc84ce01
renanguilhermef <renanguilhermef@gmail.com>
2023-02-15 22:10:48 -0300

    created private route tables and associate it to private sub nets

--- network.tf
+++ network.tf
@@ -76,6 +76,31 @@ resource "aws_route_table_association" "pub-rt-1b-associate" {
     route_table_id = aws_route_table.public-rt.id
 }
 
+resource "aws_route_table" "private-rt-1a" {
+    vpc_id = aws_vpc.vorx-vpc-prod.id 
+
+    tags = {
+        Name = "prod-private-rt-1a"
+    }
+}
+
+resource "aws_route_table" "private-rt-1b" {
+    vpc_id = aws_vpc.vorx-vpc-prod.id 
+
+    tags = {
+        Name = "prod-private-rt-1b"
+    }
+}
+
+resource "aws_route_table_association" "priv-rt-1a-associate" {
+    subnet_id = aws_subnet.vorx-subnet-priv-1a.id
+    route_table_id = aws_route_table.private-rt-1a.id
+}
+
+resource "aws_route_table_association" "priv-rt-1b-associate" {
+    subnet_id = aws_subnet.vorx-subnet-priv-1b.id
+    route_table_id = aws_route_table.private-rt-1b.id
+}
 
 ## OUTPUTS
 
@@ -110,6 +135,15 @@ output "igw_id" {
 output "public_route_table_id" {
     value = aws_route_table.public-rt.id
 }
+
+output "private_route_table_1a_id" {
+    value = aws_route_table.private-rt-1a.id
+}
+
+output "private_route_table_1b_id" {
+    value = aws_route_table.private-rt-1b.id
+}
+
 
 
 
