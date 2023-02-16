521f5f2dc5b92ae4fd59bcdb5cf4147e47146113
renanguilhermef <renanguilhermef@gmail.com>
2023-02-15 21:23:07 -0300

    add subnets

--- network.tf
+++ network.tf
@@ -6,12 +6,42 @@ resource "aws_vpc" "vorx-vpc-prod" {
     }
 }
 
+resource "aws_subnet" "vorx-subnet-pub-1a"{
+    vpc_id = aws_vpc.vorx-vpc-prod.id 
+    cidr_block = "10.0.1.0/24"
+    availability_zone = "us-east-1a"
+
+    tags = {
+        Name = "Public-Subnet-1a"
+    }
+}
+
+resource "aws_subnet" "vorx-subnet-priv-1a"{
+    vpc_id = aws_vpc.vorx-vpc-prod.id 
+    cidr_block = "10.0.10.0/24"
+    availability_zone = "us-east-1a"
+
+    tags = {
+        Name = "Private-Subnet-1a"
+    }
+}
+
+
 ## OUTPUTS
 
 output "vpc_vorx_prod_id" {
-    value = "aws_vpc.vorx-vpc-prod.id"
+    value = aws_vpc.vorx-vpc-prod.id
 }
 
 output "vpc_vorx_prod_arn" {
-    value = "aws_vpc.vorx-vpc-prod.arn"
+    value = aws_vpc.vorx-vpc-prod.arn
 }
+
+output "subnet_public_1a_id" {
+    value = aws_subnet.vorx-subnet-pub-1a.id
+}
+
+output "subnet_private_1a_id" {
+    value = aws_subnet.vorx-subnet-priv-1a.id
+}
+
